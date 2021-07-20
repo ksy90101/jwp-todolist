20628a612f205b97f0ff0f63fc551403e20af7b5
ksy90101 <ksy90101@gmail.com>
2021-07-20 12:11:18 +0900

    build: project init

---
+++ src/main/java/com/example/jwptodolist/JwpTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwptodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwpTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jwptodolist/JwpTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwptodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwpTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
