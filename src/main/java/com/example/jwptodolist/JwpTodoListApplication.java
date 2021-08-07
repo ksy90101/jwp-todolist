c1c56614316a60f4976eeeb95ef5a30ca9b26590
ksy90101 <ksy90101@gmail.com>
2021-08-07 11:04:48 +0900

    feat: baseTimeEntity 추가

--- src/main/java/com/example/jwptodolist/JwpTodoListApplication.java
+++ src/main/java/com/example/jwptodolist/JwpTodoListApplication.java
@@ -2,11 +2,13 @@ package com.example.jwptodolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JwpTodoListApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(JwpTodoListApplication.class, args);
     }
 
